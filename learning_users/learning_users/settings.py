62e5c92a6c522ba654f5849df3add05fa6e3e2de
Sagar <sagar.guliani24@gmail.com>
2018-05-31 04:40:27 +0530

    added pythonanywhere

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -26,7 +26,7 @@ MEDIA_DIR = os.path.join(BASE_DIR,'media')
 SECRET_KEY = 'niv*=75y0^l@$8e@8y@vf!3%o+aus@k$_n_6nl^sukghxksci$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["sagarguliani.pythonanywhere.com"]
 
